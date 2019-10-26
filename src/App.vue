08eaf00b6f6a469d893caf839b2fe9fa128f7123
Marcos Santos <marcosadriano034@gmail.com>
2019-10-26 17:44:47 -0300

    created actions of inserting and removing names

--- src/App.vue
+++ src/App.vue
@@ -11,30 +11,74 @@
           <div class="col-md">
             <h5>
               Prefixos
-              <span class="badge badge-info">{{ sufixes.length }}</span>
+              <span class="badge badge-info">{{ prefixes.length }}</span>
             </h5>
             <div class="card">
               <div class="card-body">
                 <ul class="list-group">
-                  <li class="list-group-item" v-for="sufix in sufixes" :key="sufix">{{ sufix }}</li>
+                  <li class="list-group-item" v-for="prefix in prefixes" :key="prefix">
+                    <div class="row">
+                      <div class="col-md">
+                        {{ prefix }}
+                      </div>
+                      <div class="col-md text-right">
+                        <button class="btn btn-info" v-on:click="deletePrefix(prefix)"><span class="fa fa-trash"></span></button>
+                      </div>
+                    </div>
+                  </li>
                 </ul>
                 <br />
-                <input class="form-control" type="text" placeholder="Digite o Prefixos" />
+                <div class="input-group">
+                  <input
+                    class="form-control"
+                    type="text"
+                    v-model="prefix"
+                    v-on:keyup.enter="addPrefix"
+                    placeholder="Digite o Prefixos"
+                  />
+                  <div class="input-group-append">
+                    <button type="button" class="btn btn-info" v-on:click="addPrefix">
+                      <span class="fa fa-plus"></span>
+                    </button>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
           <div class="col-md">
             <h5>
               Sufixos
-              <span class="badge badge-info">{{ prefixes.length }}</span>
+              <span class="badge badge-info">{{ sufixes.length }}</span>
             </h5>
             <div class="card">
               <div class="card-body">
                 <ul class="list-group">
-                  <li class="list-group-item" v-for="prefix in prefixes" :key="prefix">{{ prefix }}</li>
+                  <li class="list-group-item" v-for="sufix in sufixes" :key="sufix">
+                    <div class="row">
+                      <div class="col-md">
+                        {{ sufix }}
+                      </div>
+                      <div class="col-md text-right">
+                        <button class="btn btn-info" v-on:click="deleteSufix(sufix)"><span class="fa fa-trash"></span></button>
+                      </div>
+                    </div>
+                  </li>
                 </ul>
                 <br />
-                <input class="form-control" type="text" placeholder="Digite o Sufixos" />
+                <div class="input-group">
+                  <input
+                    class="form-control"
+                    type="text"
+                    v-model="sufix"
+                    v-on:keyup.enter="addSufix"
+                    placeholder="Digite o Sufixos"
+                  />
+                  <div class="input-group-append" v-on:click="addSufix">
+                    <button type="button" class="btn btn-info">
+                      <span class="fa fa-plus"></span>
+                    </button>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
@@ -76,8 +120,37 @@ export default {
 				"FligthHub",
 				"FligthStation",
 				"FligthMart"
-			]
+			],
+			prefix: "",
+			sufix: ""
 		};
+	},
+	methods: {
+		addPrefix() {
+			this.prefixes.push(this.prefix);
+			this.prefix = "";
+			this.generate();
+		},
+		deletePrefix(value) {
+			this.prefixes = this.prefixes.filter(prefix => prefix != value);
+			this.generate();      
+		},
+		addSufix() {
+			this.sufixes.push(this.sufix);
+			this.sufix = "";
+			this.generate();
+		},
+		deleteSufix(value) {
+			this.sufixes = this.sufixes.filter(sufix => sufix != value);
+			this.generate();
+		},
+		generate() {
+			this.domains = [];
+
+			this.prefixes.map(prefix => {
+				this.sufixes.map(sufix => this.domains.push(prefix + sufix));
+			});
+		}
 	}
 };
 </script>
